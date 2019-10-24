5721df27c56314da1dc46e6cfd1f07a70fae3aff
eunicecompra <eunicecompra@gmail.com>
2019-10-24 14:39:11 +1100

    Implemented check palindrome using a set

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -2,4 +2,15 @@
 class PalindromeChecker:
     @staticmethod
     def check_palindrome(word):
-        return True
+        result = False
+        odd_letter_set = set()
+        for letter in word:
+            if letter in odd_letter_set:
+                odd_letter_set.remove(letter)
+            else:
+                odd_letter_set.add(letter)
+
+        if len(odd_letter_set) == 1:
+            result = True
+
+        return result
--- palindrome_checker_test.py
+++ palindrome_checker_test.py
@@ -7,6 +7,10 @@ class PalindromeCheckerTest(unittest.TestCase):
     def test_palindrome_checker_class(self):
         self.assertIsInstance(PalindromeChecker(), PalindromeChecker, "class exists")
 
-    def test_check_palindrome(self):
+    def test_check_palindrome_true(self):
         result = PalindromeChecker.check_palindrome("civic")
         self.assertTrue(result)
+
+    def test_check_palindrome_false(self):
+        result = PalindromeChecker.check_palindrome("civil")
+        self.assertFalse(result)
